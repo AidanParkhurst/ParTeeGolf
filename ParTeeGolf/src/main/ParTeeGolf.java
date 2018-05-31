ce3a394852d619078f88a60cfe5d87e63f9515e8
AmaniIslam <31045723+AmaniIslam@users.noreply.github.com>
2018-05-31 13:45:54 -0400

    Testing push for amani

--- ParTeeGolf/src/main/ParTeeGolf.java
+++ ParTeeGolf/src/main/ParTeeGolf.java
@@ -2,8 +2,8 @@ package main;
 //This will presumably be our main
 public class ParTeeGolf {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args ) {
+		System.out.println("This is amani pushing");
 	}
 
 }
