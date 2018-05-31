21fdb491fa57f9bbd8ae9dbfe75c37714344a88c
aidan <bboccsgo@gmail.com>
2018-05-31 14:05:01 -0400

    Created the Frame for the game.

--- ParTeeGolf/src/main/ParTeeGolf.java
+++ ParTeeGolf/src/main/ParTeeGolf.java
@@ -1,8 +1,21 @@
 package main;
+
+import javax.swing.JFrame;
+
 //This will presumably be our main
-public class ParTeeGolf {
+public class ParTeeGolf extends JFrame {
+	
+	public ParTeeGolf() {
+		System.out.println("This is the frame starting!");
+	}
+	
 	public static void main(String[] args ) {
-		System.out.println("This is Aidan repushing!");
+		ParTeeGolf w = new ParTeeGolf();
+		w.setTitle("ParTee Golf!");
+		w.setSize(600,600);
+		w.setLocationRelativeTo(null);
+		w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		w.setVisible(true);
 	}
 
 }
